18c349057d8f7676192edd2995cf935c3d635513
Talan Harakal <tah428@lehigh.edu>
2026-09-02 20:48:46 -0400

    Jupyter works on local machine

--- hello.py
+++ hello.py
@@ -1,6 +1,10 @@
+# Python 3.14 Core
+#C:\Users\talan\AppData\Local\Python\pythoncore-3.14-64\python.exe
+
 print("Hello, World!")
 
 import numpy as np
+import sys
 
 def greet(name):
     return f"Hello, {name}!"
@@ -13,3 +17,6 @@ def random_array(size):
 print(random_array(5))
 
 print(np.std(random_array(10)))
+
+print(np.mean(random_array(10)))
+print(sys.executable)
