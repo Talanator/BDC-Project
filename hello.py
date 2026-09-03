654acbacbb2e20f00346a04f996670536be91be2
Talan Harakal <tah428@lehigh.edu>
2026-09-02 20:17:12 -0400

    Adding some functions. Test!

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
 print("Hello, World!")
+
+import numpy as np
+
+def greet(name):
+    return f"Hello, {name}!"
+
+print(greet("Sarah"))
+
+def random_array(size):
+    return np.random.rand(size)
+
+print(random_array(5))
+
+print(np.std(random_array(10)))
